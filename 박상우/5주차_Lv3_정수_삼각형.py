1fe8c56612174b1b0437884649aba8efc39c3dab
sangwu99 <sangwu0420@gmail.com>
2023-04-25 22:12:41 +0900

    update

--- 박상우/5주차_Lv3_정수_삼각형.py
+++ 박상우/5주차_Lv3_정수_삼각형.py
@@ -1,6 +1,13 @@
-class Solution:
-    def divisorGame(self, n: int) -> bool:
-        if n%2==0:
-            return True
-        else:
-            return False
+def solution(triangle):
+    answer = 0
+    n = len(triangle)
+    dp = [triangle[i] + [-1] * (n - i - 1) for i in range(n)]
+    
+    for i in range(1, n):
+        for j in range(n):
+            if j == 0:
+                dp[i][j] += dp[i - 1][j]
+            else:
+                dp[i][j] += max(dp[i-1][j], dp[i-1][j-1])
+    
+    return max(dp[-1])
